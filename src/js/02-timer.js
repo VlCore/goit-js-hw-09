69733b09f3a352c28bb4219e7d9bb4abfeabf6d4
VlCore <korovin_vladislav@icloud.com>
2023-07-11 02:19:00 +0300

    maybe final

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,93 +1,73 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from 'notiflix';
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.min.css'
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
+require("flatpickr/dist/themes/dark.css");
 
+const startBtn = document.querySelector('button[data-start]')
 const dateTimePicker = document.querySelector('#datetime-picker')
-const startBtn = document.querySelector('[data-start]')
-startBtn.disabled = true
+
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    const days = Math.floor(ms / day)
+    const hours = Math.floor((ms % day) / hour) 
+    const minutes = Math.floor(((ms % day) % hour) / minute)  
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second)
   
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds }
   }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+    let targetTime = null
+    let intervalTime = null
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        const selectedDate = selectedDates[0]
-    if (selectedDate <= new Date()) {
-        Notiflix.Notify.warning("Please choose a date in the future!")
-        startBtn.disabled = true
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0]
+    if (selectedDate < new Date()) {
+      Notify.failure('Please choose a date in the future!')
+      startBtn.disabled = true
     } else {
-    startBtn.disabled = false
+        startBtn.disabled = false
+        targetTime = selectedDate.getTime()
     }
-}
-}
+  },
+};
 
 flatpickr(dateTimePicker, options)
-
-const timerDays = document.querySelector('[data-days]')
-const timerHours = document.querySelector('[data-hours]')
-const timerMinutes = document.querySelector('[data-minutes]')
-const timerSeconds = document.querySelector('[data-seconds]')
 
 function addLeadingZero(value) {
     return value.toString().padStart(2, '0')
 }
 
-function updateTimer(days, hours, minutes, seconds) {
-    timerDays.textContent = addLeadingZero(days)
-    timerHours.textContent = addLeadingZero(hours)
-    timerMinutes.textContent = addLeadingZero(minutes)
-    timerSeconds.textContent = addLeadingZero(seconds)
+function updTimeToNew() {
+  const currentDate = new Date()
+  const difference = targetTime - currentDate
+  const convertedDifference = convertMs(difference)
+  if (difference < 0) {
+    clearInterval(intervalTime)
+    Notify.success('Time is over! ;)')
+    startBtn.disabled = false
+    dateTimePicker.disabled = false
+    return;
+  }
+
+  for (const num in convertedDifference) {
+    const numValue = convertedDifference[num]
+    document.querySelector(`[data-${num}]`).textContent = addLeadingZero(numValue)
+  }
 }
 
-function startTimer(targetDate) {
-    const intervalId = setInterval(() => {
-        const currentDate = new Date()
-        const remainingTime = targetDate.getTime() - currentDate.getTime()
+const StartBtnClick = evt => {
+  intervalTime = setInterval(updTimeToNew, 1000)
+  startBtn.disabled = true;
+  dateTimePicker.disabled = true;
+};
 
-    if (remainingTime <= 0) {
-        clearInterval(intervalId)
-        updateTimer(0, 0, 0, 0)
-        Notiflix.Notify.success('Time is over :)')
-        return
-    }
-    const { days, hours, minutes, seconds } = convertMs(remainingTime);
-    updateTimer(days, hours, minutes, seconds);
-  }, 1000);
-}
-
-startBtn.addEventListener("click", () => {
-    const selectedDate = flatpickr.parseDate(
-      dateTimePicker.value
-    )
-    if (!selectedDate) {
-      Notiflix.Notify.warning("Please choose a valid date!");
-      return;
-    }
-    
-    startTimer(selectedDate);
-    startBtn.disabled = true;
-})
-
+startBtn.addEventListener('click', StartBtnClick)
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const shouldResolve = Math.random() > 0.3
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
+}
+const formEl = document.querySelector('.form')
+
+formEl.addEventListener('submit', formElSubmit)
+
+function formElSubmit(evt) {
+  evt.preventDefault()
+  let { amount, delay, step } = evt.target.elements
+  amount = Number.parseInt(amount.value)
+  delay = Number.parseInt(delay.value)
+  step = Number.parseInt(step.value)
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i + 1, delay + step * i)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      })
   }
 }
