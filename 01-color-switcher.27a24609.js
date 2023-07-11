918f810ebd38f80de3e24497b4a74828e8341c72
VlCore <129991454+VlCore@users.noreply.github.com>
2023-07-11 09:14:25 +0000

    Deploying to gh-pages from @ VlCore/goit-js-hw-09@0f34ac2c42561597bf5f445bb2051cf578fff0bc 🚀

---
+++ 01-color-switcher.27a24609.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(t=>{const e=t.target;a||(e.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3))})),e.addEventListener("click",(t=>{const e=t.target;clearInterval(a),a=null,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.27a24609.js.map
