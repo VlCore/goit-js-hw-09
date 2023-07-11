324da2d91ef6defaad245c25af908eef17fcb0d7
VlCore <129991454+VlCore@users.noreply.github.com>
2023-07-11 20:36:39 +0000

    Deploying to gh-pages from @ VlCore/goit-js-hw-09@0f8b09ea2adb62a068124337a33e3e1abb1ce206 🚀

--- 01-color-switcher.28cfaeda.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(t=>{a||(t.target.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3))})),e.addEventListener("click",(e=>{clearInterval(a),a=null,e.target.disabled=!1,t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.28cfaeda.js.map
--- 01-color-switcher.83f0db27.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;t.addEventListener("click",(function(t){a||(t.target.disabled=!0,a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3))})),e.addEventListener("click",(function(e){clearInterval(a),a=null,e.target.disabled=!1,t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.83f0db27.js.map
